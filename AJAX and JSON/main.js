5191e8786dd6db00cb93b5cf429549df65bdacf4
Richie <richiemugambi4@gmail.com>
2021-06-01 02:32:01 -0700

    inserting data from a server to div element

--- AJAX and JSON/main.js
+++ AJAX and JSON/main.js
@@ -1,7 +1,23 @@
-var mydata= new XMLHttpRequest;
-mydata.open('GET','https://learnwebcode.github.io/json-example/animals-1.json')
+var pagecounter=1
+var button= document.getElementById("btn")
+var emptydiv= document.getElementById("pupuman")
+btn.addEventListener("click",function() {
+    var mydata= new XMLHttpRequest;
+mydata.open('GET','https://learnwebcode.github.io/json-example/animals-'+pagecounter+'.json')
 mydata.onload=function(){
     theData= JSON.parse(mydata.responseText);
-    console.log(theData[0])
+    renderhtml(theData);
 }
 mydata.send();
+pagecounter++
+if (pagecounter>3) {
+    btn.classList.add("hide-me")
+}
+})
+function renderhtml(data) {
+    var outputting="";
+    for(i=0;i<data.length;i++){
+        outputting += "<p>"+ data[i].name+ " is a "+ data[i].species+".</p>"
+    }
+    emptydiv.insertAdjacentHTML('beforeend',outputting)
+}
